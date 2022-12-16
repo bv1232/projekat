3e317e2fc9f9240b3a4763110aae33c9c8cc2845
bvreto1 <bvreto1@etf.unsa.ba>
2022-12-16 20:17:56 +0100

    Added Dao interface

---
+++ src/main/java/ba/unsa/etf/rpr/Dao.java
@@ -0,0 +1,10 @@
+package ba.unsa.etf.rpr;
+import java.util.List;
+public interface Dao<T> {
+
+    T getById(int id);
+    T add(T item);
+    T update(T item);
+    void delete(int id);
+    List<T> getAll();
+}
