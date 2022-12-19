5caa36fcd05441dbfb20d713e4029d8dc742ac32
bvreto1 <bvreto1@etf.unsa.ba>
2022-12-20 00:23:20 +0100

    Added SQLImpl classes.

---
+++ src/main/java/ba/unsa/etf/rpr/Dao/KarteDaoSQLImpl.java
@@ -0,0 +1,31 @@
+package ba.unsa.etf.rpr.Dao;
+
+import java.util.List;
+
+public class KarteDaoSQLImpl implements KarteDao{
+
+    @Override
+    public Object getById(int id) {
+        return null;
+    }
+
+    @Override
+    public Object add(Object item) {
+        return null;
+    }
+
+    @Override
+    public Object update(Object item) {
+        return null;
+    }
+
+    @Override
+    public void delete(int id) {
+
+    }
+
+    @Override
+    public List getAll() {
+        return null;
+    }
+}
---
+++ src/main/java/ba/unsa/etf/rpr/Dao/LetoviDaoSQLImp.java
@@ -0,0 +1,30 @@
+package ba.unsa.etf.rpr.Dao;
+
+import java.util.List;
+
+public class LetoviDaoSQLImp implements LetoviDao{
+    @Override
+    public Object getById(int id) {
+        return null;
+    }
+
+    @Override
+    public Object add(Object item) {
+        return null;
+    }
+
+    @Override
+    public Object update(Object item) {
+        return null;
+    }
+
+    @Override
+    public void delete(int id) {
+
+    }
+
+    @Override
+    public List getAll() {
+        return null;
+    }
+}
