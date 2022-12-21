55f5863003ffac177933c04cc762d0141dbaed7e
bvreto1 <bvreto1@etf.unsa.ba>
2022-12-21 23:38:58 +0100

    Small changes in Dao and added getById in LetoviDaoSQLImpl.

--- src/main/java/ba/unsa/etf/rpr/Dao/Dao.java
+++ src/main/java/ba/unsa/etf/rpr/Dao/Dao.java
@@ -1,8 +1,9 @@
 package ba.unsa.etf.rpr.Dao;
+import java.sql.SQLException;
 import java.util.List;
 public interface Dao<T> {
 
-    T getById(int id);
+    T getById(int id) throws SQLException;
     T add(T item);
     T update(T item);
     void delete(int id);
