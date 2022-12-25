ae4c96bfa1597f3dab1e3068a2e66f05fbce1897
bvreto1 <bvreto1@etf.unsa.ba>
2022-12-25 20:41:46 +0100

    Dao methods throw Exception

--- src/main/java/ba/unsa/etf/rpr/Dao/Dao.java
+++ src/main/java/ba/unsa/etf/rpr/Dao/Dao.java
@@ -3,9 +3,9 @@ import java.sql.SQLException;
 import java.util.List;
 public interface Dao<T> {
 
-    T getById(int id) throws SQLException;
-    T add(T item);
-    T update(T item);
-    void delete(int id);
-    List<T> getAll();
+    T getById(int id) throws Exception;
+    T add(T item) throws Exception;
+    T update(T item) throws Exception;
+    void delete(int id) throws Exception;
+    List<T> getAll() throws Exception;
 }
