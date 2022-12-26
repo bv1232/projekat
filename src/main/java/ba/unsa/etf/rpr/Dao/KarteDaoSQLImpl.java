d46176fb4fb1aca67b2d322aeddcc235f54bfea3
bvreto1 <bvreto1@etf.unsa.ba>
2022-12-26 21:43:09 +0100

    Applied row2object and object2row in KarteDaoSQLImpl

--- src/main/java/ba/unsa/etf/rpr/Dao/KarteDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/Dao/KarteDaoSQLImpl.java
@@ -1,31 +1,39 @@
 package ba.unsa.etf.rpr.Dao;
 
-import java.util.List;
+import ba.unsa.etf.rpr.Classes.Karta;
 
-public class KarteDaoSQLImpl implements KarteDao{
+import java.sql.*;
+import java.util.*;
 
-    @Override
-    public Object getById(int id) {
-        return null;
+public class KarteDaoSQLImpl extends AbstractDao<Karta> implements KarteDao{
+
+    public KarteDaoSQLImpl() {
+        super("karta");
     }
 
     @Override
-    public Object add(Object item) {
-        return null;
+    public Karta row2object(ResultSet rs) throws Exception {
+        try {
+            Karta karta = new Karta();
+            karta.setId(rs.getInt("id"));
+            karta.setLet(DaoFactory.letoviDao().getById(rs.getInt("idLeta")));
+            karta.setKlasa(rs.getString("klasa"));
+            karta.setPutnik(DaoFactory.putniciDao().getById(rs.getInt("idPutnika")));
+            karta.setSjediste(rs.getString("sjediste"));
+            return karta;
+        } catch (SQLException e) {
+            throw new Exception(e.getMessage(), e);
+        }
     }
-
     @Override
-    public Object update(Object item) {
-        return null;
+    public Map<String, Object> object2row(Karta object) {
+        Map<String, Object> row = new TreeMap<>();
+        row.put("id", object.getId());
+        row.put("idLeta", object.getLet().getId());
+        row.put("klasa", object.getKlasa());
+        row.put("idPutnika", object.getPutnik().getId());
+        row.put("sjediste", object.getSjediste());
+        return row;
     }
 
-    @Override
-    public void delete(int id) {
-
-    }
-
-    @Override
-    public List getAll() {
-        return null;
-    }
 }
