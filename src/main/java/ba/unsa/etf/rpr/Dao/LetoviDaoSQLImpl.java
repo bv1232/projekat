c44197973b2e298925a43fdd800e2dc3300724b7
bvreto1 <bvreto1@etf.unsa.ba>
2022-12-26 21:56:47 +0100

    Implemented row2object and object2row in LetoviDaoSQLImpl

--- src/main/java/ba/unsa/etf/rpr/Dao/LetoviDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/Dao/LetoviDaoSQLImpl.java
@@ -2,7 +2,6 @@ package ba.unsa.etf.rpr.Dao;
 
 import ba.unsa.etf.rpr.Classes.Karta;
 import ba.unsa.etf.rpr.Classes.Let;
-import sun.security.x509.OtherName;
 
 import java.io.IOException;
 import java.sql.*;
@@ -11,88 +10,37 @@ import java.util.Map;
 import java.util.Properties;
 import java.util.TreeMap;
 
-public class LetoviDaoSQLImpl implements LetoviDao{
+public class LetoviDaoSQLImpl extends AbstractDao<Let> implements LetoviDao{
 
-    private Connection connection;
-    private String tableName;
-    public LetoviDaoSQLImpl(String tableName) throws IOException {
-        try {
-            this.tableName = tableName;
-            Properties p = new Properties();
-            p.load(ClassLoader.getSystemResource("app.properties").openStream());
-            String url = p.getProperty("db.connection_string");
-            String username = p.getProperty("db.username");
-            String password = p.getProperty("db.password");
-            this.connection = DriverManager.getConnection(url, username, password);
-        }catch (Exception e){
-            System.out.println("Nemoguce uspostaviti konekciju sa bazom");
-            e.printStackTrace();
-        }
+    public LetoviDaoSQLImpl() {
+        super("letovi");
     }
 
-    public Connection getConnection() {
-        return this.connection;
-    }
-    public Object row2objects(ResultSet rs) throws SQLException {
+    @Override
+    public Let row2object(ResultSet rs) throws Exception {
         try {
             Let let = new Let();
             let.setId(rs.getInt("id"));
-            let.setPocetnaDestinacija(rs.getString("pocetna destinacija"));
-            let.setKrajnjaDestinacija(rs.getString("krajnja destinacija"));
             let.setDatum(rs.getDate("datum"));
             let.setVrijemePolaska(rs.getTime("polazak"));
-            let.setPaviljon(rs.getString("paviljon"));
+            let.setPocetnaDestinacija(rs.getString("pocetna destinacija"));
+            let.setKrajnjaDestinacija(rs.getString("krajnja destinacija"));
+            let.setTerminal(rs.getString("terminal"));
             return let;
-        }catch (SQLException e){
-            throw new SQLException(e.getMessage(),e);
-        }
-    }
-    public Map<String,Object> object2row(Let object){
-        Map<String,Object> item = new TreeMap<String,Object>();
-        item.put("id",object.getId());
-        item.put("pocetna destinacija", object.getPocetnaDestinacija());
-        item.put("krajnja destinacija", object.getKrajnjaDestinacija());
-        item.put("datum", object.getDatum());
-        item.put("polazak", object.getVrijemePolaska());
-        item.put("paviljon", object.getPaviljon());
-        return item;
-    }
-    @Override
-    public Object getById(int id) throws SQLException {
-        String query = "SELECT * FROM " + this.tableName + " WHERE id = ?";
-        try {
-            PreparedStatement stmt = this.connection.prepareStatement(query);
-            stmt.setInt(1, id);
-            ResultSet rs = stmt.executeQuery();
-            if(rs.next()){
-                Object result = row2objects(rs);
-                rs.close();
-                return result;
-            }else{
-                throw new SQLException("Objekat nije pronadjen");
-            }
-        }catch (SQLException e){
-            throw new SQLException(e.getMessage(),e);
+        } catch (SQLException e) {
+            throw new Exception(e.getMessage(), e);
         }
     }
 
     @Override
-    public Object add(Object item) {
-        return null;
-    }
-
-    @Override
-    public Object update(Object item) {
-        return null;
-    }
-
-    @Override
-    public void delete(int id) {
-
-    }
-
-    @Override
-    public List getAll() {
-        return null;
+    public Map<String, Object> object2row(Let object) {
+        Map<String, Object> row = new TreeMap<>();
+        row.put("id", object.getId());
+        row.put("datum", object.getDatum());
+        row.put("polazak", object.getVrijemePolaska());
+        row.put("pocetna destinacija", object.getPocetnaDestinacija());
+        row.put("krajnja destinacija", object.getKrajnjaDestinacija());
+        row.put("terminal", object.getTerminal());
+        return row;
     }
 }
