3c24bf89251313c4fd39853286eff4c959a136e3
bvreto1 <bvreto1@etf.unsa.ba>
2022-12-28 23:38:36 +0100

    Added fxml dependencies and plugins, VM options and login.fxml file

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -1,13 +1,30 @@
 package ba.unsa.etf.rpr;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
+public class App extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/login.fxml"));
+        primaryStage.setTitle("Zdravo");
+        primaryStage.setScene(new Scene(root, USE_COMPUTED_SIZE,USE_COMPUTED_SIZE));
+        primaryStage.show();
+    }
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+        launch(args);
     }
 }
