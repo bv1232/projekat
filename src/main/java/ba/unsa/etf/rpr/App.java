e381bf83e45fb5117209cd85e820d80aa24f8c6c
bvreto1 <bvreto1@etf.unsa.ba>
2023-01-12 01:57:37 +0100

    Minor change

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -17,7 +17,7 @@ public class App extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/login.fxml"));
-        primaryStage.setTitle("Zdravo");
+        primaryStage.setTitle("Login");
         primaryStage.setScene(new Scene(root, USE_COMPUTED_SIZE,USE_COMPUTED_SIZE));
         primaryStage.setResizable(false);
         primaryStage.show();
