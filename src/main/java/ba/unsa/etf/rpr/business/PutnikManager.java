2347a64b8fdde41653d7edec2936afe4b23b9e3c
bvreto1 <bvreto1@etf.unsa.ba>
2023-01-12 01:35:07 +0100

    Implemented PutnikManager.

--- src/main/java/ba/unsa/etf/rpr/business/PutnikManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/PutnikManager.java
@@ -1,4 +1,36 @@
 package ba.unsa.etf.rpr.business;
 
+import ba.unsa.etf.rpr.Dao.DaoFactory;
+import ba.unsa.etf.rpr.Domain.Putnik;
+import ba.unsa.etf.rpr.Exception.KartaException;
+
+import java.util.List;
+
 public class PutnikManager {
+    public Putnik add(Putnik putnik) throws KartaException {
+        try {
+            return DaoFactory.putniciDao().add(putnik);
+        } catch (KartaException e) {
+            throw new KartaException(e.getMessage(),e);
+        }
+    }
+    public void delete(int putnikId) throws KartaException{
+        try {
+            DaoFactory.putniciDao().delete(putnikId);
+        } catch (KartaException e) {
+            throw new KartaException(e.getMessage(),e);
+        }
+    }
+
+    public Putnik update(Putnik putnik) throws KartaException {
+       return DaoFactory.putniciDao().update(putnik);
+    }
+    public Putnik getById(int id) throws KartaException{
+        return DaoFactory.putniciDao().getById(id);
+    }
+
+    public List<Putnik> getAll() throws KartaException{
+        return DaoFactory.putniciDao().getAll();
+    }
+
 }
