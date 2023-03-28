ddc8ba483415a3dc0878de20354da52e5d6333b8
bvreto1 <bvreto1@etf.unsa.ba>
2023-03-29 00:52:53 +0200

    Implemented cancelButton function

--- src/main/java/ba/unsa/etf/rpr/Controllers/RegistracijaController.java
+++ src/main/java/ba/unsa/etf/rpr/Controllers/RegistracijaController.java
@@ -8,6 +8,7 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class RegistracijaController {
 
@@ -43,6 +44,7 @@ public class RegistracijaController {
         }
     }
     public void cancelButton(ActionEvent actionEvent){
-        
+        Stage stage = (Stage) cancelButtonId.getScene().getWindow();
+        stage.close();
     }
 }
