e8eb7bdc2335ed92e9e6f783217ffeb50870ce18
bvreto1 <bvreto1@etf.unsa.ba>
2023-03-30 00:07:04 +0200

    Added logInId function

--- src/main/java/ba/unsa/etf/rpr/Dao/PutniciDao.java
+++ src/main/java/ba/unsa/etf/rpr/Dao/PutniciDao.java
@@ -1,6 +1,8 @@
 package ba.unsa.etf.rpr.Dao;
 
 import ba.unsa.etf.rpr.Domain.Putnik;
+import ba.unsa.etf.rpr.Exception.KartaException;
 
 public interface PutniciDao extends Dao<Putnik> {
+    int logInId(String username, String password)  throws KartaException;
 }
