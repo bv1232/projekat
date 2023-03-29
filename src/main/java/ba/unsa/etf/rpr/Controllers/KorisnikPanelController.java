ec6051d58542793e69fa0562fe18634ee11883da
bvreto1 <bvreto1@etf.unsa.ba>
2023-03-29 20:03:29 +0200

    Minor change

--- src/main/java/ba/unsa/etf/rpr/Controllers/KorisnikPanelController.java
+++ src/main/java/ba/unsa/etf/rpr/Controllers/KorisnikPanelController.java
@@ -1,4 +1,17 @@
 package ba.unsa.etf.rpr.Controllers;
 
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+
 public class KorisnikPanelController {
+
+    public DatePicker datePickerId;
+    public Label usernameLabelId;
+    public TextField brKartiId;
+
+    public RadioButton prvaKlasaId;
+    public RadioButton biznisKlasaId;
+    public RadioButton ekonomskaKlasaId;
 }
