41e516fd7cebf3308ce967450da8eeeedde2bb99
bvreto1 <bvreto1@etf.unsa.ba>
2023-03-29 03:13:17 +0200

    Added an alert

--- src/main/java/ba/unsa/etf/rpr/Controllers/LoginController.java
+++ src/main/java/ba/unsa/etf/rpr/Controllers/LoginController.java
@@ -4,10 +4,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Hyperlink;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -22,6 +19,8 @@ public class LoginController {
     public Button okButtonId;
 
     public void cancelButtonClick (ActionEvent actionEvent){
+        Alert alert = new Alert(Alert.AlertType.WARNING);
+        alert.showAndWait();
         Stage stage = (Stage) cancelButtonId.getScene().getWindow();
         stage.close();
     }
