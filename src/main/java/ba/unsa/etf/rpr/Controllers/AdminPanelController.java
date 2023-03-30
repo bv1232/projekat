64931471c770c120e915440640198cbfa50f394f
bvreto1 <bvreto1@etf.unsa.ba>
2023-03-31 01:35:11 +0200

    Added public attributes

--- src/main/java/ba/unsa/etf/rpr/Controllers/AdminPanelController.java
+++ src/main/java/ba/unsa/etf/rpr/Controllers/AdminPanelController.java
@@ -1,4 +1,23 @@
 package ba.unsa.etf.rpr.Controllers;
 
+import javafx.scene.control.*;
+
 public class AdminPanelController {
+    public TableView letoviTableId;
+    public TableColumn polazisteColId;
+    public TableColumn krajnjaColId;
+    public TableColumn datumColId;
+    public TableColumn vrijemeColId;
+    public TableColumn terminalColId;
+    public TextField polazisteId;
+    public TextField krajnjaId;
+    public DatePicker datumId;
+    public TextField vrijemeId;
+    public TextField terminalId;
+    public Button addButtonId;
+    public Button updateButtonId;
+    public Button deleteButtonId;
+    public Button logOutButtonId;
+    public Tab putniciTabId;
+    public Tab letoviTabId;
 }
