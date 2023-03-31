e912ce9c386ee7b8c20dd0bb112436fb51d46d29
bvreto1 <bvreto1@etf.unsa.ba>
2023-03-31 03:58:46 +0200

    Added getByKrajnjaDestinacija

--- src/main/java/ba/unsa/etf/rpr/Dao/LetoviDao.java
+++ src/main/java/ba/unsa/etf/rpr/Dao/LetoviDao.java
@@ -1,6 +1,8 @@
 package ba.unsa.etf.rpr.Dao;
 
 import ba.unsa.etf.rpr.Domain.Let;
+import ba.unsa.etf.rpr.Exception.KartaException;
 
 public interface LetoviDao extends Dao<Let>{
+    Let getByKrajnjaDestinacija(String krajnja) throws KartaException;
 }
