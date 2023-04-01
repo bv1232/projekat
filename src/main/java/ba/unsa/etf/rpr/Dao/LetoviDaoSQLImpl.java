966e08c48ce15a9c0b070f42874ab199e393a002
bvreto1 <bvreto1@etf.unsa.ba>
2023-04-01 03:04:29 +0200

    Quick fix

--- src/main/java/ba/unsa/etf/rpr/Dao/LetoviDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/Dao/LetoviDaoSQLImpl.java
@@ -20,7 +20,7 @@ public class LetoviDaoSQLImpl extends AbstractDao<Let> implements LetoviDao{
             Let let = new Let();
             let.setId(rs.getInt("id"));
             let.setDatum(rs.getDate("datum"));
-            let.setVrijemePolaska(rs.getDate("polazak"));
+            let.setVrijemePolaska(rs.getTime("polazak"));
             let.setPocetnaDestinacija(rs.getString("pocetna destinacija"));
             let.setKrajnjaDestinacija(rs.getString("krajnja destinacija"));
             let.setTerminal(rs.getString("terminal"));
