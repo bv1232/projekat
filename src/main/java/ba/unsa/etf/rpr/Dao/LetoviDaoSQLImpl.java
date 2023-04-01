ddbb29985a84878786da5a946b5d742eed8b5cad
bvreto1 <bvreto1@etf.unsa.ba>
2023-04-01 03:44:42 +0200

    Refactoring

--- src/main/java/ba/unsa/etf/rpr/Dao/LetoviDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/Dao/LetoviDaoSQLImpl.java
@@ -21,8 +21,8 @@ public class LetoviDaoSQLImpl extends AbstractDao<Let> implements LetoviDao{
             let.setId(rs.getInt("id"));
             let.setDatum(rs.getDate("datum"));
             let.setVrijemePolaska(rs.getTime("polazak"));
-            let.setPocetnaDestinacija(rs.getString("pocetna destinacija"));
-            let.setKrajnjaDestinacija(rs.getString("krajnja destinacija"));
+            let.setPocetnaDestinacija(rs.getString("pocetnaDestinacija"));
+            let.setKrajnjaDestinacija(rs.getString("krajnjaDestinacija"));
             let.setTerminal(rs.getString("terminal"));
             return let;
         } catch (SQLException e) {
@@ -34,10 +34,10 @@ public class LetoviDaoSQLImpl extends AbstractDao<Let> implements LetoviDao{
     public Map<String, Object> object2row(Let object) {
         Map<String, Object> row = new TreeMap<>();
         row.put("id", object.getId());
+        row.put("pocetnaDestinacija", object.getPocetnaDestinacija());
+        row.put("krajnjaDestinacija", object.getKrajnjaDestinacija());
         row.put("datum", object.getDatum());
         row.put("polazak", object.getVrijemePolaska());
-        row.put("pocetna destinacija", object.getPocetnaDestinacija());
-        row.put("krajnja destinacija", object.getKrajnjaDestinacija());
         row.put("terminal", object.getTerminal());
         return row;
     }
